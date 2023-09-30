6ae97bfc1c2329c6a9d09b8bd972d371d7b4e687
Lir Zeitouny <lirzetouny@gmail.com>
2023-09-30 09:34:19 +0300

    adding axios

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
 </template>
 
 <script lang="ts">
+import axios from 'axios';
+
+// Configure Axios base URL
 import { defineComponent } from 'vue';
+axios.defaults.baseURL = 'http://localhost:3000';
 
 export default defineComponent({
   name: 'App',
